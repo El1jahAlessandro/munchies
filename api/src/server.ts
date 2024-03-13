3748b48275790e8e460a02d0f463a143919109d6
Elijah Freimuth <elijah.freimuth@konzepthaus-ws.de>
2024-03-13 11:43:17 +0100

    add dotenv

--- api/src/server.ts
+++ api/src/server.ts
@@ -1,12 +1,19 @@
 import express from 'express'
 import allRouter from "./routers/_all.router";
+import * as dotenv from 'dotenv';
+
+dotenv.config({path: '../api/.env', debug: true});
 
 const app = express()
-const PORT = 6000
+const port = process.env.PORT
 
-app.use(express.json())
-app.use('/api', allRouter)
+if (port) {
+    app.use(express.json())
+    app.use('/api', allRouter)
 
-app.listen(PORT, () =>
-    console.log(`🚀 Server ready at: http://localhost:${PORT}`),
-)
+    app.listen(port, () =>
+        console.log(`🚀 Server ready at: http://localhost:${port}`),
+    )
+} else {
+    console.error("\x1b[31m", '!!!--- Port is not defined ---!!!')
+}
