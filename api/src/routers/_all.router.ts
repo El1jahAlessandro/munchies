52d0b58c9c87de1f3469e0e9b6de00e3079ce1ac
Elijah Freimuth <elijah.freimuth@konzepthaus-ws.de>
2024-04-02 09:21:06 +0200

    add user endpoint

--- api/src/routers/_all.router.ts
+++ api/src/routers/_all.router.ts
@@ -1,6 +1,8 @@
 import express from "express";
 import testRouter from "./test/test.router";
+import userRouter from "./user/user.router";
 
 const allRouter = express.Router()
 allRouter.use('/test', testRouter)
+allRouter.use('/user', userRouter)
 export default allRouter
---
+++ api/src/routers/user/create.ts
@@ -0,0 +1,18 @@
+import {z} from "zod";
+import {getZodParams} from "../../helpers/getZodParams";
+import {asyncExpressHandler, StatusError} from "../../helpers/asyncExpressHandler";
+
+const queriesSchema = z.object({
+    name: z.string(getZodParams("string")),
+    age: z.preprocess(value => value ? Number(value) : value, z.number(getZodParams("number"))),
+})
+
+export const create = asyncExpressHandler(
+    async ({query: unknownQueries}, res) => {
+        const {name, age} = queriesSchema.parse(unknownQueries)
+        if (age < 18) {
+            throw new StatusError(401, "You are under 18, you are not authorized")
+        }
+        res.status(200).json(`Hello ${name}, you are ${age} years old`)
+    }
+)
