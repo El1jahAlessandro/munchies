9b8a76fa96cf3ea512587bff7cd5884e6cb092fb
Elijah Freimuth <elijah.freimuth@konzepthaus-ws.de>
2024-04-24 17:58:56 +0200

    add profile pic input in profile page

--- src/app/(profile)/profile/page.tsx
+++ src/app/(profile)/profile/page.tsx
@@ -1,10 +1,35 @@
 'use client';
 
 import ProfilePic from '@/components/ProfilePic/ProfilePic';
-import { Badge } from '@mui/material';
+import { Badge, IconButton } from '@mui/material';
 import { CameraAlt } from '@mui/icons-material';
+import { ChangeEvent, useEffect, useMemo } from 'react';
+import { Controller, useForm } from 'react-hook-form';
+import { useUserContext } from '@/components/hooks/userContext';
+import { editUserBodySchema, EditUserBodyType } from '@/lib/schemas/user.schema';
+import { pick } from 'lodash';
+import { User } from '@prisma/client';
 
 export default function ProfilePage() {
+    const { user } = useUserContext();
+    const {
+        reset,
+        getValues,
+        control,
+        watch,
+        formState: { errors, isDirty, dirtyFields },
+    } = useForm<EditUserBodyType>({
+        defaultValues: useMemo(
+            () => pick(user as User, editUserBodySchema.omit({ profilePic: true }).keyof().options),
+            [user]
+        ),
+    });
+
+    useEffect(() => {
+        reset(pick(user as User, editUserBodySchema.omit({ profilePic: true }).keyof().options));
+    }, [user]);
+
+    console.log(getValues());
     return (
         <>
             <div
@@ -15,17 +40,38 @@ export default function ProfilePage() {
                 }}
             >
                 <div style={{ marginTop: '50px' }}>
-                    <Badge
-                        anchorOrigin={{
-                            vertical: 'bottom',
-                            horizontal: 'right',
-                        }}
-                        badgeContent={<CameraAlt sx={{ width: '10px' }} />}
-                        color={'secondary'}
-                        overlap="circular"
-                    >
-                        <ProfilePic width={100} height={100} />
-                    </Badge>
+                    <Controller
+                        name="profilePic"
+                        control={control}
+                        render={({ field: { value, onChange, ...field } }) => (
+                            <>
+                                <input
+                                    id={'contained-button-file'}
+                                    type="file"
+                                    hidden
+                                    {...field}
+                                    onChange={(event: ChangeEvent<HTMLInputElement>) =>
+                                        onChange(event.target.files?.[0])
+                                    }
+                                />
+                                <label htmlFor="contained-button-file">
+                                    <IconButton component="span">
+                                        <Badge
+                                            anchorOrigin={{
+                                                vertical: 'bottom',
+                                                horizontal: 'right',
+                                            }}
+                                            badgeContent={<CameraAlt sx={{ width: '10px' }} />}
+                                            color={'secondary'}
+                                            overlap="circular"
+                                        >
+                                            <ProfilePic width={100} height={100} />
+                                        </Badge>
+                                    </IconButton>
+                                </label>
+                            </>
+                        )}
+                    />
                 </div>
             </div>
         </>
--- src/lib/schemas/user.schema.ts
+++ src/lib/schemas/user.schema.ts
@@ -3,20 +3,21 @@ import { accountTypeSchema } from '@/lib/schemas/common.schema';
 
 export type AuthUserBodyType = z.infer<typeof authUserBodySchema>;
 export type CreateUserBodyType = z.infer<typeof createUserInputSchema>;
+export type EditUserBodyType = z.infer<typeof editUserBodySchema>;
 
 export const authUserBodySchema = z.object({
     email: z.string().email(),
     password: z.string(),
 });
 
-export const createUserBodySchema = authUserBodySchema.merge(
-    z.object({
-        accountType: accountTypeSchema,
-        profilePic: z.instanceof(File).nullish(),
-        forename: z.string().nullish(),
-        lastname: z.string().nullish(),
-    })
-);
+export const editUserBodySchema = z.object({
+    accountType: accountTypeSchema,
+    profilePic: z.instanceof(File).nullish(),
+    forename: z.string().nullish(),
+    lastname: z.string().nullish(),
+});
+
+export const createUserBodySchema = authUserBodySchema.merge(editUserBodySchema);
 
 export const createUserInputSchema = createUserBodySchema
     .merge(
