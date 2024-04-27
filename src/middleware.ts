fac714ddf9218be967192e3d2816a043161d6727
Elijah Freimuth <elijah.freimuth@konzepthaus-ws.de>
2024-04-27 22:46:45 +0200

    fix middleware + update packages

--- src/middleware.ts
+++ src/middleware.ts
@@ -1,19 +1,16 @@
-import { NextRequest } from 'next/server';
-import { pages } from '@/lib/utils/routes';
+import { NextRequest, NextResponse } from 'next/server';
+import { authorizationCookieName } from '@/lib/utils/constants';
 
 export function middleware(req: NextRequest) {
-    // TODO: fix this
-    /*const isAuthenticated = req.cookies.has(authorizationCookieName);
-
+    const isAuthenticated = req.cookies.has(authorizationCookieName);
     // If the user is authenticated or on an login/register page, continue as normal
     if (isAuthenticated || ['/login', '/register', '/api'].some(page => req.nextUrl.pathname.startsWith(page))) {
         return NextResponse.next();
     }
-
     // Redirect to login page if not authenticated
-    return NextResponse.redirect(new URL('/login', req.url));*/
+    return NextResponse.redirect(new URL('/login', req.url));
 }
 
 export const config = {
-    matcher: ['/((?!_next|api).*)(.+)', pages.home],
+    matcher: ['/((?!_next|api|login|register).*)(.+)'],
 };
