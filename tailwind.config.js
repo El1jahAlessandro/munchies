fcf6dec7bd0315b0d9eb0c73abd01d128ed4600f
Elijah Freimuth <elijah.freimuth@konzepthaus-ws.de>
2024-06-06 21:55:35 +0200

    config tailwind + postcss

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,14 +1,22 @@
-/** @type {import('tailwindcss').Config} */
-const tailwindConfig = {
-    corePlugins: {
-        preflight: false,
-    },
-    content: ['./app/**/*.{js,ts,jsx,tsx,mdx}', './components/**/*.{js,ts,jsx,tsx,mdx}'],
-    prefix: '',
+/** @type {Theme | {readonly default: Theme}} */
+module.exports = {
+    content: ['./src/**/*.{js,ts,jsx,tsx,mdx}'],
     theme: {
+        colors: {
+            primary: {
+                main: '#00a023',
+            },
+            secondary: {
+                main: '#9796A1',
+            },
+            error: {
+                main: '#d32f2f',
+            },
+            white: '#ffffff',
+            black: '#000000',
+            'traffic-white': '#F6F6F6',
+        },
         extend: {},
     },
     plugins: [],
 };
-
-export default tailwindConfig;
