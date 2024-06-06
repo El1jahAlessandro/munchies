c6c45b4dd3951b9ad433967e68df136408f6ab75
Elijah Freimuth <elijah.freimuth@konzepthaus-ws.de>
2024-06-07 00:08:47 +0200

    adjust side menu styling + change translation

--- src/components/SideMenu/SideMenu.tsx
+++ src/components/SideMenu/SideMenu.tsx
@@ -13,11 +13,6 @@ import {
 import MenuIcon from '@mui/icons-material/Menu';
 import TextSnippetIcon from '@mui/icons-material/TextSnippet';
 import PersonIcon from '@mui/icons-material/Person';
-import LocationOnIcon from '@mui/icons-material/LocationOn';
-import PaymentIcon from '@mui/icons-material/Payment';
-import EmailIcon from '@mui/icons-material/Email';
-import SettingsIcon from '@mui/icons-material/Settings';
-import HelpIcon from '@mui/icons-material/Help';
 import PowerSettingsNewIcon from '@mui/icons-material/PowerSettingsNew';
 import { NextLinkComposed } from '@/components/common/NextLinkComposed';
 import { useUserContext } from '@/components/hooks/userContext';
@@ -30,44 +25,6 @@ import { ButtonComponent } from '@/components/common/ButtonComponent';
 
 type ToggleDrawerType = (newOpen: boolean) => () => void;
 
-const menuList = [
-    {
-        icon: <TextSnippetIcon />,
-        label: 'My Orders',
-        href: pages.orders,
-    },
-    {
-        icon: <PersonIcon />,
-        label: 'My Profile',
-        href: pages.profile,
-    },
-    {
-        icon: <LocationOnIcon />,
-        label: 'Delivery Address',
-        href: pages.home,
-    },
-    {
-        icon: <PaymentIcon />,
-        label: 'Payment Methods',
-        href: pages.home,
-    },
-    {
-        icon: <EmailIcon />,
-        label: 'Contact Us',
-        href: pages.home,
-    },
-    {
-        icon: <SettingsIcon />,
-        label: 'Settings',
-        href: pages.home,
-    },
-    {
-        icon: <HelpIcon />,
-        label: 'Helps & FAQs',
-        href: pages.home,
-    },
-];
-
 export function MenuButton({ toggleDrawer }: { toggleDrawer: ToggleDrawerType }) {
     return (
         <Button onClick={toggleDrawer(true)}>
@@ -76,7 +33,7 @@ export function MenuButton({ toggleDrawer }: { toggleDrawer: ToggleDrawerType })
     );
 }
 
-function LogOutButton({}) {
+function LogOutButton() {
     const { mutate: userMutate } = useUserContext();
     const { mutate: cartMutate } = useCartContext();
     const { push } = useRouter();
@@ -104,23 +61,62 @@ function LogOutButton({}) {
 
 export default function SideMenu({ open, toggleDrawer }: { open: boolean; toggleDrawer: ToggleDrawerType }) {
     const { user } = useUserContext();
+
+    const menuList = [
+        {
+            icon: <TextSnippetIcon />,
+            label: user?.accountType === 'user' ? 'Meine Bestellungen' : 'Eingehende Bestellungen',
+            href: pages.orders,
+        },
+        {
+            icon: <PersonIcon />,
+            label: 'Mein Profil',
+            href: pages.profile,
+        },
+        /*{
+            icon: <LocationOnIcon />,
+            label: 'Delivery Address',
+            href: pages.home,
+        },
+        {
+            icon: <PaymentIcon />,
+            label: 'Payment Methods',
+            href: pages.home,
+        },
+        {
+            icon: <EmailIcon />,
+            label: 'Contact Us',
+            href: pages.home,
+        },
+        {
+            icon: <SettingsIcon />,
+            label: 'Settings',
+            href: pages.home,
+        },
+        {
+            icon: <HelpIcon />,
+            label: 'Helps & FAQs',
+            href: pages.home,
+        },*/
+    ];
+
     return (
         <Drawer open={open} onClose={toggleDrawer(false)}>
             <div>
-                <ProfilePic width={100} height={100} className={'ml-2 mt-[20px]'} />
-                <Typography variant="subtitle1" component="div" className={'font-bold pl-2 pt-[20px]'}>
+                <ProfilePic width={100} height={100} className={'ml-4 mt-5'} />
+                <Typography variant="subtitle1" component="div" className={'font-bold pl-4 pt-5'}>
                     {user?.name}
                 </Typography>
-                <Typography variant="caption" component="div" className={'pl-2 text-secondary-main'}>
+                <Typography variant="caption" component="div" className={'pl-4 text-secondary-main'}>
                     {user?.email}
                 </Typography>
             </div>
-            <Box className={'w-[250px] mt-[20px]'} role="presentation" onClick={toggleDrawer(false)}>
+            <Box className={'w-[250px] mt-5'} role="presentation" onClick={toggleDrawer(false)}>
                 <List>
                     {menuList.map(({ label, icon, href }) => (
                         <ListItem key={label} disablePadding component={NextLinkComposed} to={href}>
                             <ListItemButton>
-                                <ListItemIcon>{icon}</ListItemIcon>
+                                <ListItemIcon className={'min-w-10'}>{icon}</ListItemIcon>
                                 <ListItemText className={'text-secondary-main'} primary={label} />
                             </ListItemButton>
                         </ListItem>
